4475f978e145fd1c45ae6820f5a67f30039c5f6e
Amjad Alsharafi <26300843+Amjad50@users.noreply.github.com>
2023-12-25 21:19:11 +0800

    Amjad50/OS: added very basic single threaded `thread_local_key`
    This is only to make it work with `stdout` and `ReMutex`

--- library/std/src/sys/amjad_os/thread_local_key.rs
+++ library/std/src/sys/amjad_os/thread_local_key.rs
@@ -1,21 +1,27 @@
 pub type Key = usize;
 
+// FIXME: this is only for 1 single thread at a time
+static mut DATA: *mut u8 = core::ptr::null_mut();
+
 #[inline]
 pub unsafe fn create(_dtor: Option<unsafe extern "C" fn(*mut u8)>) -> Key {
-    panic!("should not be used on this target");
+    // panic!("should not be used on this target");
+    1
 }
 
 #[inline]
 pub unsafe fn set(_key: Key, _value: *mut u8) {
-    panic!("should not be used on this target");
+    // panic!("should not be used on this target");
+    unsafe { DATA = _value };
 }
 
 #[inline]
 pub unsafe fn get(_key: Key) -> *mut u8 {
-    panic!("should not be used on this target");
+    // panic!("should not be used on this target");
+    unsafe { DATA }
 }
 
 #[inline]
 pub unsafe fn destroy(_key: Key) {
-    panic!("should not be used on this target");
+    // panic!("should not be used on this target");
 }
