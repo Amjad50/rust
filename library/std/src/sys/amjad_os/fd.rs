4d2e8607a37cc86c99fad87c4d2e627d8cf83f19
Amjad Alsharafi <26300843+Amjad50@users.noreply.github.com>
2023-12-25 21:17:41 +0800

    Amjad50/OS: added `read`/`write` to `fd` and `stdio`
    Most taken from `unix`, but most are commented and just used my functions provided by `user_std`
    Check: https://github.com/Amjad50/OS/commit/4d5fcc3096ea1625d4cf04bf31800c8ab4b786e2

---
+++ library/std/src/sys/amjad_os/fd.rs
@@ -0,0 +1,272 @@
+#![unstable(reason = "not public", issue = "none", feature = "fd")]
+
+use crate::io::{self, BorrowedCursor, IoSlice, IoSliceMut, Read};
+// use crate::os::unix::io::{AsFd, AsRawFd, BorrowedFd, FromRawFd, IntoRawFd, OwnedFd, RawFd};
+// use crate::sys_common::{AsInner, FromInner, IntoInner};
+
+use super::syscall_to_io_error;
+
+// TODO: add `ownedFd` and other fd types to manage dropping them
+#[derive(Debug)]
+pub struct FileDesc(usize);
+
+// The maximum read limit on most POSIX-like systems is `SSIZE_MAX`,
+// with the man page quoting that if the count of bytes to read is
+// greater than `SSIZE_MAX` the result is "unspecified".
+//
+// On macOS, however, apparently the 64-bit libc is either buggy or
+// intentionally showing odd behavior by rejecting any read with a size
+// larger than or equal to INT_MAX. To handle both of these the read
+// size is capped on both platforms.
+#[allow(dead_code)]
+const READ_LIMIT: usize = isize::MAX as usize;
+
+#[allow(dead_code)]
+const fn max_iov() -> usize {
+    16 // The minimum value required by POSIX.
+}
+
+impl FileDesc {
+    pub fn from_raw_fd(fd: usize) -> Self {
+        Self(fd)
+    }
+
+    pub fn read(&self, buf: &mut [u8]) -> io::Result<usize> {
+        // let ret = cvt(unsafe {
+        //     libc::read(
+        //         self.as_raw_fd(),
+        //         buf.as_mut_ptr() as *mut libc::c_void,
+        //         cmp::min(buf.len(), READ_LIMIT),
+        //     )
+        // })?;
+        // Ok(ret as usize)
+        let ret = unsafe { user_std::io::syscall_read(self.0, buf).map_err(syscall_to_io_error)? };
+        Ok(ret as usize)
+    }
+
+    pub fn read_vectored(&self, bufs: &mut [IoSliceMut<'_>]) -> io::Result<usize> {
+        io::default_read_vectored(|b| self.read(b), bufs)
+    }
+
+    #[inline]
+    pub fn is_read_vectored(&self) -> bool {
+        false
+    }
+
+    pub fn read_to_end(&self, buf: &mut Vec<u8>) -> io::Result<usize> {
+        let mut me = self;
+        (&mut me).read_to_end(buf)
+    }
+
+    pub fn read_at(&self, buf: &mut [u8], offset: u64) -> io::Result<usize> {
+        // unsafe {
+        //     cvt(pread64(
+        //         self.as_raw_fd(),
+        //         buf.as_mut_ptr() as *mut libc::c_void,
+        //         cmp::min(buf.len(), READ_LIMIT),
+        //         offset as off64_t,
+        //     ))
+        //     .map(|n| n as usize)
+        // }
+        todo!()
+    }
+
+    pub fn read_buf(&self, mut cursor: BorrowedCursor<'_>) -> io::Result<()> {
+        // let ret = cvt(unsafe {
+        //     libc::read(
+        //         self.as_raw_fd(),
+        //         cursor.as_mut().as_mut_ptr() as *mut libc::c_void,
+        //         cmp::min(cursor.capacity(), READ_LIMIT),
+        //     )
+        // })?;
+
+        // // Safety: `ret` bytes were written to the initialized portion of the buffer
+        // unsafe {
+        //     cursor.advance(ret as usize);
+        // }
+        // Ok(())
+        todo!()
+    }
+
+    pub fn read_vectored_at(&self, bufs: &mut [IoSliceMut<'_>], offset: u64) -> io::Result<usize> {
+        io::default_read_vectored(|b| self.read_at(b, offset), bufs)
+    }
+
+    pub fn write(&self, buf: &[u8]) -> io::Result<usize> {
+        // let ret = cvt(unsafe {
+        //     libc::write(
+        //         self.as_raw_fd(),
+        //         buf.as_ptr() as *const libc::c_void,
+        //         cmp::min(buf.len(), READ_LIMIT),
+        //     )
+        // })?;
+        // Ok(ret as usize)
+        let ret = unsafe { user_std::io::syscall_write(self.0, buf).map_err(syscall_to_io_error)? };
+        Ok(ret as usize)
+    }
+
+    pub fn write_vectored(&self, bufs: &[IoSlice<'_>]) -> io::Result<usize> {
+        io::default_write_vectored(|b| self.write(b), bufs)
+    }
+
+    #[inline]
+    pub fn is_write_vectored(&self) -> bool {
+        false
+    }
+
+    pub fn write_at(&self, buf: &[u8], offset: u64) -> io::Result<usize> {
+        // #[cfg(not(any(
+        //     all(target_os = "linux", not(target_env = "musl")),
+        //     target_os = "android",
+        //     target_os = "hurd"
+        // )))]
+        // use libc::pwrite as pwrite64;
+        // #[cfg(any(
+        //     all(target_os = "linux", not(target_env = "musl")),
+        //     target_os = "android",
+        //     target_os = "hurd"
+        // ))]
+        // use libc::pwrite64;
+
+        // unsafe {
+        //     cvt(pwrite64(
+        //         self.as_raw_fd(),
+        //         buf.as_ptr() as *const libc::c_void,
+        //         cmp::min(buf.len(), READ_LIMIT),
+        //         offset as off64_t,
+        //     ))
+        //     .map(|n| n as usize)
+        // }
+        todo!()
+    }
+
+    pub fn write_vectored_at(&self, bufs: &[IoSlice<'_>], offset: u64) -> io::Result<usize> {
+        io::default_write_vectored(|b| self.write_at(b, offset), bufs)
+    }
+
+    #[cfg(not(any(
+        target_env = "newlib",
+        target_os = "solaris",
+        target_os = "illumos",
+        target_os = "emscripten",
+        target_os = "fuchsia",
+        target_os = "l4re",
+        target_os = "linux",
+        target_os = "haiku",
+        target_os = "redox",
+        target_os = "vxworks",
+        target_os = "nto",
+    )))]
+    pub fn set_cloexec(&self) -> io::Result<()> {
+        // unsafe {
+        //     cvt(libc::ioctl(self.as_raw_fd(), libc::FIOCLEX))?;
+        //     Ok(())
+        // }
+        todo!()
+    }
+    #[cfg(any(
+        all(
+            target_env = "newlib",
+            not(any(target_os = "espidf", target_os = "horizon", target_os = "vita"))
+        ),
+        target_os = "solaris",
+        target_os = "illumos",
+        target_os = "emscripten",
+        target_os = "fuchsia",
+        target_os = "l4re",
+        target_os = "linux",
+        target_os = "haiku",
+        target_os = "redox",
+        target_os = "vxworks",
+        target_os = "nto",
+    ))]
+    pub fn set_cloexec(&self) -> io::Result<()> {
+        unsafe {
+            let previous = cvt(libc::fcntl(self.as_raw_fd(), libc::F_GETFD))?;
+            let new = previous | libc::FD_CLOEXEC;
+            if new != previous {
+                cvt(libc::fcntl(self.as_raw_fd(), libc::F_SETFD, new))?;
+            }
+            Ok(())
+        }
+    }
+
+    #[cfg(target_os = "linux")]
+    pub fn set_nonblocking(&self, nonblocking: bool) -> io::Result<()> {
+        // unsafe {
+        //     let v = nonblocking as libc::c_int;
+        //     cvt(libc::ioctl(self.as_raw_fd(), libc::FIONBIO, &v))?;
+        //     Ok(())
+        // }
+        todo!()
+    }
+
+    #[inline]
+    pub fn duplicate(&self) -> io::Result<FileDesc> {
+        // Ok(Self(self.0.try_clone()?))
+        todo!()
+    }
+}
+
+impl<'a> Read for &'a FileDesc {
+    fn read(&mut self, buf: &mut [u8]) -> io::Result<usize> {
+        (**self).read(buf)
+    }
+
+    fn read_buf(&mut self, cursor: BorrowedCursor<'_>) -> io::Result<()> {
+        (**self).read_buf(cursor)
+    }
+
+    fn read_vectored(&mut self, bufs: &mut [IoSliceMut<'_>]) -> io::Result<usize> {
+        (**self).read_vectored(bufs)
+    }
+
+    #[inline]
+    fn is_read_vectored(&self) -> bool {
+        (**self).is_read_vectored()
+    }
+}
+
+// impl AsInner<OwnedFd> for FileDesc {
+//     #[inline]
+//     fn as_inner(&self) -> &OwnedFd {
+//         &self.0
+//     }
+// }
+
+// impl IntoInner<OwnedFd> for FileDesc {
+//     fn into_inner(self) -> OwnedFd {
+//         self.0
+//     }
+// }
+
+// impl FromInner<OwnedFd> for FileDesc {
+//     fn from_inner(owned_fd: OwnedFd) -> Self {
+//         Self(owned_fd)
+//     }
+// }
+
+// impl AsFd for FileDesc {
+//     fn as_fd(&self) -> BorrowedFd<'_> {
+//         self.0.as_fd()
+//     }
+// }
+
+// impl AsRawFd for FileDesc {
+//     #[inline]
+//     fn as_raw_fd(&self) -> RawFd {
+//         self.0.as_raw_fd()
+//     }
+// }
+
+// impl IntoRawFd for FileDesc {
+//     fn into_raw_fd(self) -> RawFd {
+//         self.0.into_raw_fd()
+//     }
+// }
+
+// impl FromRawFd for FileDesc {
+//     unsafe fn from_raw_fd(raw_fd: RawFd) -> Self {
+//         Self(FromRawFd::from_raw_fd(raw_fd))
+//     }
+// }
--- library/std/src/sys/amjad_os/stdio.rs
+++ library/std/src/sys/amjad_os/stdio.rs
@@ -1,30 +1,56 @@
-use crate::io;
+use core::{io::BorrowedCursor, mem::ManuallyDrop};
 
-pub struct Stdin;
-pub struct Stdout;
-pub struct Stderr;
+use crate::io::{self, IoSlice, IoSliceMut};
+
+use super::fd::FileDesc;
+
+pub struct Stdin(());
+pub struct Stdout(());
+pub struct Stderr(());
 
 impl Stdin {
     pub const fn new() -> Stdin {
-        Stdin
+        Stdin(())
     }
 }
 
 impl io::Read for Stdin {
-    fn read(&mut self, _buf: &mut [u8]) -> io::Result<usize> {
-        Ok(0)
+    fn read(&mut self, buf: &mut [u8]) -> io::Result<usize> {
+        ManuallyDrop::new(FileDesc::from_raw_fd(user_std::io::FD_STDIN)).read(buf)
+    }
+
+    fn read_buf(&mut self, buf: BorrowedCursor<'_>) -> io::Result<()> {
+        ManuallyDrop::new(FileDesc::from_raw_fd(user_std::io::FD_STDIN)).read_buf(buf)
+    }
+
+    fn read_vectored(&mut self, bufs: &mut [IoSliceMut<'_>]) -> io::Result<usize> {
+        ManuallyDrop::new(FileDesc::from_raw_fd(user_std::io::FD_STDIN)).read_vectored(bufs)
+    }
+
+    #[inline]
+    fn is_read_vectored(&self) -> bool {
+        ManuallyDrop::new(FileDesc::from_raw_fd(user_std::io::FD_STDIN)).is_read_vectored()
     }
 }
 
 impl Stdout {
     pub const fn new() -> Stdout {
-        Stdout
+        Stdout(())
     }
 }
 
 impl io::Write for Stdout {
     fn write(&mut self, buf: &[u8]) -> io::Result<usize> {
-        Ok(buf.len())
+        ManuallyDrop::new(FileDesc::from_raw_fd(user_std::io::FD_STDOUT)).write(buf)
+    }
+
+    fn write_vectored(&mut self, bufs: &[IoSlice<'_>]) -> io::Result<usize> {
+        ManuallyDrop::new(FileDesc::from_raw_fd(user_std::io::FD_STDOUT)).write_vectored(bufs)
+    }
+
+    #[inline]
+    fn is_write_vectored(&self) -> bool {
+        ManuallyDrop::new(FileDesc::from_raw_fd(user_std::io::FD_STDOUT)).is_write_vectored()
     }
 
     fn flush(&mut self) -> io::Result<()> {
@@ -34,13 +60,22 @@ impl io::Write for Stdout {
 
 impl Stderr {
     pub const fn new() -> Stderr {
-        Stderr
+        Stderr(())
     }
 }
 
 impl io::Write for Stderr {
     fn write(&mut self, buf: &[u8]) -> io::Result<usize> {
-        Ok(buf.len())
+        ManuallyDrop::new(FileDesc::from_raw_fd(user_std::io::FD_STDERR)).write(buf)
+    }
+
+    fn write_vectored(&mut self, bufs: &[IoSlice<'_>]) -> io::Result<usize> {
+        ManuallyDrop::new(FileDesc::from_raw_fd(user_std::io::FD_STDERR)).write_vectored(bufs)
+    }
+
+    #[inline]
+    fn is_write_vectored(&self) -> bool {
+        ManuallyDrop::new(FileDesc::from_raw_fd(user_std::io::FD_STDERR)).is_write_vectored()
     }
 
     fn flush(&mut self) -> io::Result<()> {
@@ -54,6 +89,6 @@ pub fn is_ebadf(_err: &io::Error) -> bool {
     true
 }
 
-pub fn panic_output() -> Option<Vec<u8>> {
-    None
+pub fn panic_output() -> Option<impl io::Write> {
+    Some(Stderr::new())
 }
