208a4fd5a1aeca5ef7756736155815f41577e1a9
Amjad Alsharafi <26300843+Amjad50@users.noreply.github.com>
2023-12-25 19:12:05 +0800

    Amjad50/OS: added new target os: `amjad_os` and impl `alloc`

---
+++ library/std/src/sys/amjad_os/alloc.rs
@@ -0,0 +1,25 @@
+use crate::alloc::{GlobalAlloc, Layout, System};
+use crate::ptr::null_mut;
+
+#[stable(feature = "alloc_system_type", since = "1.28.0")]
+unsafe impl GlobalAlloc for System {
+    #[inline]
+    unsafe fn alloc(&self, layout: Layout) -> *mut u8 {
+        unsafe { user_std::alloc::alloc(layout) }
+    }
+
+    #[inline]
+    unsafe fn alloc_zeroed(&self, layout: Layout) -> *mut u8 {
+        unsafe { user_std::alloc::alloc_zeroed(layout) }
+    }
+
+    #[inline]
+    unsafe fn dealloc(&self, ptr: *mut u8, layout: Layout) {
+        unsafe { user_std::alloc::dealloc(ptr, layout) }
+    }
+
+    #[inline]
+    unsafe fn realloc(&self, ptr: *mut u8, layout: Layout, new_size: usize) -> *mut u8 {
+        unsafe { user_std::alloc::realloc(ptr, layout, new_size) }
+    }
+}
