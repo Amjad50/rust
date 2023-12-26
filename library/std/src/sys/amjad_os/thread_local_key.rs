7e94f56fb930622dce4c7bda9e8b14dcc78f6aa7
Amjad Alsharafi <26300843+Amjad50@users.noreply.github.com>
2023-12-26 15:46:51 +0800

    Amjad50/OS: changed `thread_local` to support more than 1 local a time.
    It was causing weird crashes haha

--- library/std/src/sys/amjad_os/thread_local_key.rs
+++ library/std/src/sys/amjad_os/thread_local_key.rs
@@ -1,27 +1,36 @@
+use core::sync::atomic::{AtomicUsize, Ordering};
+
 pub type Key = usize;
 
 // FIXME: this is only for 1 single thread at a time
-static mut DATA: *mut u8 = core::ptr::null_mut();
+static mut MAP: [*mut u8; 256] = [core::ptr::null_mut(); 256];
+static MAP_INDEX: AtomicUsize = AtomicUsize::new(0);
 
 #[inline]
 pub unsafe fn create(_dtor: Option<unsafe extern "C" fn(*mut u8)>) -> Key {
     // panic!("should not be used on this target");
-    1
+    let index = MAP_INDEX.fetch_add(1, Ordering::SeqCst);
+    index
 }
 
 #[inline]
-pub unsafe fn set(_key: Key, _value: *mut u8) {
+pub unsafe fn set(key: Key, value: *mut u8) {
     // panic!("should not be used on this target");
-    unsafe { DATA = _value };
+    unsafe {
+        MAP[key] = value;
+    }
 }
 
 #[inline]
-pub unsafe fn get(_key: Key) -> *mut u8 {
+pub unsafe fn get(key: Key) -> *mut u8 {
     // panic!("should not be used on this target");
-    unsafe { DATA }
+    unsafe { MAP[key] }
 }
 
 #[inline]
-pub unsafe fn destroy(_key: Key) {
+pub unsafe fn destroy(key: Key) {
     // panic!("should not be used on this target");
+    unsafe {
+        MAP[key] = core::ptr::null_mut();
+    }
 }
