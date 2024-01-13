c450aebefaafe0e99e8ffbd9a0c2131b7c269701
Amjad Alsharafi <26300843+Amjad50@users.noreply.github.com>
2024-01-13 23:22:04 +0800

    Amjad50/OS: added support for `chdir`, `getcwd`, and `current_exe`
    We can manipulate working directories now, also as a bonus from the kernel now, file operations work with relative paths :)

--- library/std/src/sys/amjad_os/os.rs
+++ library/std/src/sys/amjad_os/os.rs
@@ -1,9 +1,14 @@
+use user_std::SyscallError;
+
 use crate::error::Error as StdError;
 use crate::ffi::{OsStr, OsString};
 use crate::fmt;
 use crate::io;
 use crate::marker::PhantomData;
+use crate::os::amjad_os::prelude::OsStringExt;
 use crate::path::{self, PathBuf};
+use crate::sys::amjad_os::syscall_to_io_error;
+use crate::sys::common::small_c_string::run_path_with_cstr;
 
 // This function is needed by the panic runtime. The symbol is named in
 // pre-link args for the target specification, so keep that in sync.
@@ -29,11 +34,35 @@ pub fn error_string(_errno: i32) -> String {
 }
 
 pub fn getcwd() -> io::Result<PathBuf> {
-    todo!("getcwd")
+    let mut buf = Vec::with_capacity(512);
+    loop {
+        unsafe {
+            // Safety: the size is equal to the capacity, I'm setting the length so we can access the slice
+            //         its safe to use since the data will be overwritten by getcwd
+            buf.set_len(buf.capacity());
+            match user_std::io::syscall_get_cwd(&mut buf) {
+                Ok(len) => {
+                    // Safety: forcing the length back to the safe space
+                    buf.set_len(len);
+                    buf.shrink_to_fit();
+                    return Ok(PathBuf::from(OsString::from_vec(buf)));
+                }
+                Err(SyscallError::BufferTooSmall) => {
+                    // Trigger the internal buffer resizing logic of `Vec` by requiring
+                    // more space than the current capacity.
+                    // at this stage we have already set the length to the capacity
+                    buf.reserve(1);
+                }
+                Err(e) => return Err(syscall_to_io_error(e)),
+            }
+        }
+    }
 }
 
-pub fn chdir(_: &path::Path) -> io::Result<()> {
-    todo!("chdir")
+pub fn chdir(p: &path::Path) -> io::Result<()> {
+    run_path_with_cstr(p, |p| unsafe {
+        user_std::io::syscall_chdir(p).map_err(syscall_to_io_error)
+    })
 }
 
 pub struct SplitPaths<'a>(!, PhantomData<&'a ()>);
@@ -74,7 +103,19 @@ impl StdError for JoinPathsError {
 }
 
 pub fn current_exe() -> io::Result<PathBuf> {
-    todo!("current_exe")
+    use crate::env;
+    use crate::io::ErrorKind;
+
+    let exe_path = env::args().next().ok_or(io::const_io_error!(
+        ErrorKind::Uncategorized,
+        "an executable path was not found because no arguments were provided through argv"
+    ))?;
+    let path = PathBuf::from(exe_path);
+
+    // Prepend the current working directory to the path if it's not absolute.
+    // TODO: `is_absolute` is broken for non-unix since it relies on `cfgs` which I don't want to modify,
+    //       I'm opening a PR to fix that, then this will be changed to `is_absolute` but doesn't change much anyway
+    if !path.has_root() { getcwd().map(|cwd| cwd.join(path)) } else { Ok(path) }
 }
 
 pub struct Env(!);
